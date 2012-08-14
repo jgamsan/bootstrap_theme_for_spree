bfc12ff7045a4d5b65dd1f2995ca13cb85df4f76
jose miguel <miguel.gamazo@galiclick.com>
2012-08-14 06:40:58 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PruebaThemeSpree::Application.config.secret_token = '945ca61d14947fa5c683625e2bd519a94b72d2747e5fd49f53c4a97d2cd9411c74675478fcab7da232fc701df0b6ef8aba6111ce8fe033016b20c55383a8b9a5'
